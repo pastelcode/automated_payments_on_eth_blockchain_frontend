6700a2df82f7d97f8a6b25e4c039523730dd15c7
Pastel <pastel.dev.gt@gmail.com>
2022-11-18 02:54:22 -0600

    Add a tooltip to lapseds option when is disabled

--- lib/features/home/presentation/widgets/home_page/lapseds.dart
+++ lib/features/home/presentation/widgets/home_page/lapseds.dart
@@ -12,52 +12,57 @@ class _Lapseds extends StatelessWidget {
         BuildContext context,
         ContractSettingsState state,
       ) {
-        return AbsorbPointer(
-          absorbing: state.members.isEmpty,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              if (state.members.isEmpty)
-                Chip(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(
-                        ApplicationTheme.borderRadius,
+        return Tooltip(
+          message: state.members.isEmpty ? 'Set members first' : '',
+          child: AbsorbPointer(
+            absorbing: state.members.isEmpty,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                if (state.members.isEmpty)
+                  Chip(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(
+                          ApplicationTheme.borderRadius,
+                        ),
                       ),
                     ),
+                    label: const Text(
+                      'Set members first',
+                    ),
+                    labelStyle: Theme.of(
+                      context,
+                    ).textTheme.caption!.copyWith(
+                          color: Theme.of(
+                            context,
+                          ).colorScheme.onPrimary,
+                        ),
+                    backgroundColor: Theme.of(
+                      context,
+                    ).colorScheme.primary,
                   ),
-                  label: const Text(
-                    'Set members first',
+                Opacity(
+                  opacity: state.members.isEmpty ? .5 : 1,
+                  child: Row(
+                    children: <Widget>[
+                      const Text(
+                        'Lapseds',
+                      ),
+                      const Spacer(),
+                      Button(
+                        onPressed: () {},
+                        title: Text(
+                          state.lapseds.isEmpty ? 'Set up' : 'View',
+                        ),
+                        tooltip:
+                            state.members.isEmpty ? 'Set members first' : '',
+                      ),
+                    ],
                   ),
-                  labelStyle: Theme.of(
-                    context,
-                  ).textTheme.caption!.copyWith(
-                        color: Theme.of(
-                          context,
-                        ).colorScheme.onPrimary,
-                      ),
-                  backgroundColor: Theme.of(
-                    context,
-                  ).colorScheme.primary,
                 ),
-              Opacity(
-                opacity: state.members.isEmpty ? .5 : 1,
-                child: Row(
-                  children: <Widget>[
-                    const Text(
-                      'Lapseds',
-                    ),
-                    const Spacer(),
-                    Button(
-                      onPressed: () {},
-                      title: Text(
-                        state.lapseds.isEmpty ? 'Set up' : 'View',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
