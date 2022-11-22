e6ebdf4425094afe35951db6c231c34b25555cab
Pastel <pastel.dev.gt@gmail.com>
2022-11-22 13:52:50 -0600

    Create a function to launch uris
    The package `url_launcher` is used to achieve it

---
+++ lib/core/utils/launch_uri.dart
@@ -0,0 +1,23 @@
+import 'package:automated_payments_on_eth_blockchain_frontend/core/presentation/widgets/custom_snack_bar/custom_snack_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// Launches [uri] in the default device browser.
+///
+/// If [uri] can't be launched, it shows a snackbar with an error message.
+Future<void> launchUri({
+  required BuildContext context,
+  required Uri uri,
+}) async {
+  if (!await canLaunchUrl(uri)) {
+    CustomSnackBar.showInformationSnackBar(
+      content: Text(
+        '$uri',
+      ),
+    );
+  }
+  await launchUrl(
+    uri,
+    mode: LaunchMode.externalApplication,
+  );
+}
